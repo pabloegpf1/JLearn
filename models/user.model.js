370ed8b36eb7c5bde6d25495fdac2ccd4617d1c0
Matt M <matt@puroxy.org>
2019-04-19 15:07:43 -0700

    basic models created

---
+++ models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = userSchema;
