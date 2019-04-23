bfee92a1714facb3f4b3bf5dd743c80af096ac46
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-04-23 09:57:59 -0700

    Added redirect for non authenticated users

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,6 +1,9 @@
 const Course = require('../models/course.model')
 
 module.exports.list = (req, res) => {
+	if (!req.user) {
+		res.redirect('/login')
+	}
 	Course.find({})
 		.then((courses) => res
 			.render('pages/ilearn-homepage', {
